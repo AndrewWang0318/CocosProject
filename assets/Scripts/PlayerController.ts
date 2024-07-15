8eb8c6e3ba42fd670a4b17945fa84ba60dcd3b7f
WangNing <AndrewWang0318@outlook.com>
2024-07-15 17:59:15 +0800

    提交所有

---
+++ assets/Scripts/PlayerController.ts
@@ -0,0 +1,68 @@
+import { _decorator, Component, Node, input, Input, EventMouse, Vec3 } from 'cc';
+
+
+const { ccclass, property } = _decorator;
+
+@ccclass('PlayerController')
+export class PlayerController extends Component {
+
+  private _startJump: boolean = false; // 是否接受到跳跃命令
+  private _jumpStep: number = 0; // 跳跃步长 
+
+  private _curJumpTime:number = 0; // 当前跳跃时间
+  private _jumpTime:number = 0.1; // 每次跳跃时间  
+  private _curJumpSpeed:number = 0; // 当前跳跃速度
+
+  private _curPos: Vec3 = new Vec3(); // 当前角色位置
+  private _deltaPos: Vec3 = new Vec3(0,0,0); // 跳跃过程中,当前帧移动位置差
+  private _targetPos: Vec3 = new Vec3(); // 目标角色位置
+
+
+
+  start() {
+    input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this)
+  }
+
+  onMouseUp(evt: EventMouse) {
+    const btnCode: number = evt.getButton();
+    if (btnCode === 0) { // 按下了左键
+      this.jumpByStep(1)
+    } else if (btnCode === 2) { // 按下了右键
+      this.jumpByStep(2)
+    }
+  }
+
+  jumpByStep(step:number){
+    if (this._startJump)  return; // 上一步未完成之时不可再次触发
+
+    this._startJump = true;
+    this._jumpStep = step;
+    this._curJumpTime = 0;
+    this._curJumpSpeed = this._jumpStep / this._jumpTime;
+    this.node.getPosition(this._curPos); // 将当前节点的位置储存到this._curPos中
+
+    Vec3.add(this._targetPos,this._curPos,new Vec3(this._jumpStep, 0 , 0))
+  }
+
+  update(dt: number): void { // 60帧代表每秒执行60次
+    if(this._startJump){
+      this._curJumpTime += dt
+
+      if(this._curJumpTime > this._jumpTime){ // 跳跃结束
+
+        this.node.setPosition(this._targetPos); // 移动到目标位置
+        this._startJump = false; // 标记跳跃结束
+
+      }else{ // 跳跃中
+
+        this.node.getPosition(this._curPos); // 获取当前的位置
+        this._deltaPos.x = this._curJumpSpeed * dt;
+        Vec3.add(this._curPos, this._curPos, this._deltaPos)
+        this.node.setPosition(this._curPos);
+
+      }
+    }
+  }
+}
+
+
